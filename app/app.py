a76acc74184b99a0dc5a282f29875474c53bb518
abdullahjattak24-doctom <abdullahjattak24@gmail.com>
2026-08-04 00:20:44 -0700

    Day 10 - added SQLite database and messages page

--- app/app.py
+++ app/app.py
@@ -1,23 +1,73 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request
+import sqlite3
 
-app = Flask(
-    __name__,
-    template_folder=os.path.join(os.path.dirname(__file__), "templates"),
-    static_folder=os.path.join(os.path.dirname(__file__), "static")
-)
+app = Flask(__name__)
+
+# Create Database
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS contacts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT,
+        message TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)",
+            (name, email, message)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return "<h1>Data Saved Successfully!</h1><a href='/'>Back</a>"
+
     return render_template("contact.html")
+
+
+@app.route("/messages")
+def messages():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM contacts")
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return render_template("messages.html", contacts=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
